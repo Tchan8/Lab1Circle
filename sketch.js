824ba753c517625d2ff73562bf7d155946120c64
Tchan8 <84159100+Tchan8@users.noreply.github.com>
2021-08-23 21:20:08 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(100);
+  } else {
+    fill(200);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
